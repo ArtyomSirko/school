5e597a6caaddb0cd9b07a9b47fb033f747d1fd43
Артём <asmatrix-1@mail.ru>
2022-10-26 19:29:14 +0300

    сделал домашнию работу 3.2

---
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -0,0 +1,43 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.service.StudentService;
+
+@RequestMapping("/student")
+@RestController
+public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+    @PostMapping
+    public ResponseEntity createUser(@RequestBody Student student) {
+        Student createdUser = studentService.createUser(student);
+        return ResponseEntity.ok(createdUser);
+    }
+    @GetMapping("/{userId}")
+    public ResponseEntity getUser(@PathVariable Long userId) {
+        Student student = studentService.getUserById(userId);
+        if(student == null) {
+            return ResponseEntity.notFound() .build();
+        }
+        return ResponseEntity.ok(student);
+    }
+    @PutMapping()
+    public ResponseEntity updateUser(@RequestBody Student user) {
+        Student updatedUser = studentService.updateUser(user.getId(), user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity deleteUser(@PathVariable Long userId) {
+        Student student = studentService.deleteUser(userId);
+        if(student == null) {
+            return ResponseEntity.notFound() .build();
+        }
+        return ResponseEntity.ok(student);
+    }
+}
