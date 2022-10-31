e5f4c730cea8005f729c296d25ae19215639cb67
Артём <asmatrix-1@mail.ru>
2022-10-31 20:00:22 +0300

    сделал ДЗ 3.3

--- src/main/java/ru/hogwarts/school/repositories/FacultyRepository.java
+++ src/main/java/ru/hogwarts/school/repositories/FacultyRepository.java
@@ -1,8 +1,7 @@
 package ru.hogwarts.school.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import ru.hogwarts.school.model.Faculty;
-@Repository
+
 public interface FacultyRepository extends JpaRepository<Faculty, Long> {
 }
